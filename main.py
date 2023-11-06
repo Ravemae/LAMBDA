4863a9a25c3e6d638af55f9524ccfeaf57029361
Ravemae <veraadaezeezeanya2@gmail.com>
2023-11-06 10:54:27 +0100

    git add

---
+++ main.py
@@ -0,0 +1,146 @@
+# x = lambda a, b , c : a + b + c
+# print(x(3, 5, 7))
+#
+# def myfunc(n):
+#     return lambda a: a * n
+#
+# doubler = myfunc(5)
+# print(doubler(11))
+# print(doubler(12))
+
+
+# URL = "https://www.google.com"
+# print(URL)
+# enemies = 1
+
+# def increase_enemies():
+#     print(f"This is the enemies strength {enemies}")
+#     return enemies + 1
+
+# enemies = increase_enemies()
+# print(f"This is the enemies strength {enemies}")
+# print(enemies)
+
+def drink_potion():
+    potion_strength = 2
+
+    print(potion_strength)
+
+drink_potion()
+
+player_health = 10
+def game():
+    def drink_potion():
+        potion_strength = 2
+        print(player_health)
+    drink_potion()
+game()
+
+if 3 > 2 :
+    x = "fine"
+def my_func():
+        print(x)
+my_func()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
